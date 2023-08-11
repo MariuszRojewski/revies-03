b5ff983a5554cb36879a4a8428184e1f3a64efa8
Mariusz Rojewski <rojewski@webkon.eu>
2023-08-11 12:54:45 +0200

    02 RW 02

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,71 @@ const App = () => {
   const [index, setIndex] = React.useState(0);
   const { name, job, image, text } = people[index];
 
+  const checkNumber = (number) => {
+      if(number > people.length -1) {
+        return 0
+      } 
+
+      if(number < 0) {
+        return people.length -1;
+      }
+
+      return number;
+  }
+
+  const prevPerson = () => {
+    setIndex((prevIndex) => {
+      // if(prevIndex !== 0) {
+      //   const currentIndex = prevIndex - 1;
+      //   return currentIndex
+      // } else {
+      //   return people.length -1
+      // }
+
+      const currentIndex = prevIndex - 1;
+      return checkNumber(currentIndex)
+    })
+  }
+
+  const nextPerson = () => {
+    setIndex((prevIndex) => {
+      // if(prevIndex !== people.length -1) {
+      //   const currentIndex = prevIndex + 1;
+      //   return currentIndex
+      // } else {
+      //   return 0
+      // }
+
+      const currentIndex = prevIndex + 1;
+      return checkNumber(currentIndex)
+    })
+  }
+
   return (
     <main>
       <article className="review">
         <div className="img-container">
           <img src={image} alt={name} className="person-img"/>
-          <span className="quote-icon"><FaChevronRight /></span>
+          <span className="quote-icon"><FaQuoteRight /></span>
         </div>
         <h4 className="author">{name}</h4>
         <p className="job">{job}</p>
         <p className="info">{text}</p>
       </article>
+      <div className='btn-container'>
+        <button 
+          className='prev-btn' 
+          onClick={prevPerson}
+        >
+          <FaChevronLeft />
+        </button>
+        <button 
+          className='next-btn' 
+          onClick={nextPerson}
+        >
+          <FaChevronRight />
+        </button>
+      </div>
     </main>
   );
 };
